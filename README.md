06a9cf26b6859d5ee3ac1e575ac8c8f21e19f599
John Jones <jmjatlanta@gmail.com>
2023-06-03 14:04:18 -0500

    name clarification in itch

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ The data feed runs on port  12001
 
 The data feed (for now) is a raw ITCH feed
 
+The idea is to mock what Nasdaq ME does for Latinex so as to make realistic simulations for testing and demonstrations
+
+### Next steps
+
+The next layer above should connect to this FIX interface for order entry operations, as well as to ITCH for keeping
+track of ALL transactions on the exchange. This is the way to build a historical database as well as data feeds for
+their clients.
--- src/data_feed_client.h
+++ src/data_feed_client.h
@@ -46,7 +46,7 @@ class DataFeedClient
     virtual bool onOrderDelete(const itch::order_delete& in) { return true; }
     virtual bool onOrderReplace(const itch::order_replace& in) { return true; }
     virtual bool onTrade(const itch::trade& in) { return true; }
-    virtual bool onTradeNonCross(const itch::trade_non_cross& in) { return true; }
+    virtual bool onCrossTrade(const itch::cross_trade& in) { return true; }
     virtual bool onBrokenTrade(const itch::broken_trade& in) { return true; }
     virtual bool onNoii(const itch::noii& in) { return true; }
     virtual bool onRpii( const itch::rpii& in) { return true; }
@@ -89,49 +89,49 @@ class DataFeedClient
         const unsigned char* record = bytes.data();
         switch(bytes[0]) {
             case 'S':
-                return onSystemEvent( itch::system_event(record) );
+                return onSystemEvent( itch::system_event(record) ); // system events
             case 'R':
-                return onStockDirectory( itch::stock_directory(record));
+                return onStockDirectory( itch::stock_directory(record)); // at beginning of day, status of stocks
             case 'H':
-                return onStockTradingAction( itch::stock_trading_action(record) );
+                return onStockTradingAction( itch::stock_trading_action(record) ); // trading status of a stock, assume H (halted)
             case 'Y':
-                return onRegShoRestriction( itch::reg_sho_restriction(record) );
+                return onRegShoRestriction( itch::reg_sho_restriction(record) ); // Reg SHO toggle (off, on, still on)
             case 'L':
-                return onMarketParticipantPosition( itch::market_participant_position(record) );
+                return onMarketParticipantPosition( itch::market_participant_position(record) ); // info about market makers
             case 'V':
-                return onMwcpDeclineLevel( itch::mwcp_decline_level(record) );
+                return onMwcpDeclineLevel( itch::mwcp_decline_level(record) ); // what the marketwide circuit breaker is for the day
             case 'W':
-                return onMwcpStatus( itch::mwcp_status(record) );
+                return onMwcpStatus( itch::mwcp_status(record) ); // used when an MWCB has been reached
             case 'K':
-                return onIpoQuotingPeriodUpdate( itch::ipo_quoting_period_update(record) );
+                return onIpoQuotingPeriodUpdate( itch::ipo_quoting_period_update(record) ); // IPO
             case 'J':
-                return onLuldAuctionCollar( itch::luld_auction_collar(record) );
+                return onLuldAuctionCollar( itch::luld_auction_collar(record) ); // Limit Up / Limit Down collar info
             case 'h':
-                return onOperationalHalt( itch::operational_halt( record ) );
+                return onOperationalHalt( itch::operational_halt( record ) ); // operational halt
             case 'A':
-                return onAddOrder( itch::add_order( record ) );
+                return onAddOrder( itch::add_order( record ) ); // order
             case 'F':
-                return onAddOrderWithMpid( itch::add_order_with_mpid(record) );
+                return onAddOrderWithMpid( itch::add_order_with_mpid(record) ); // order with MPID
             case 'E':
-                return onOrderExecuted( itch::order_executed(record) );
+                return onOrderExecuted( itch::order_executed(record) ); // order executed
             case 'C':
-                return onOrderExecutedWithPrice( itch::order_executed_with_price(record) );
+                return onOrderExecutedWithPrice( itch::order_executed_with_price(record) ); // price differs from original order
             case 'X':
-                return onOrderCancel( itch::order_cancel(record) );
+                return onOrderCancel( itch::order_cancel(record) ); // order cancelled
             case 'D':
-                return onOrderDelete( itch::order_delete(record) );
+                return onOrderDelete( itch::order_delete(record) ); // order deleted
             case 'U':
-                return onOrderReplace( itch::order_replace(record) );
+                return onOrderReplace( itch::order_replace(record) ); // order replaced
             case 'P':
-                return onTrade( itch::trade(record) );
+                return onTrade( itch::trade(record) ); // a trade happened with no maching order (order was non displayable)
             case 'Q':
-                return onTradeNonCross( itch::trade_non_cross(record) );
+                return onCrossTrade( itch::cross_trade(record) ); // a trade happened, not sure when this gets sent out
             case 'B':
-                return onBrokenTrade( itch::broken_trade(record) );
+                return onBrokenTrade( itch::broken_trade(record) ); // broken trade
             case 'I':
-                return onNoii( itch::noii(record) );
-            case 'N':
-                return onRpii( itch::rpii(record) );
+                return onNoii( itch::noii(record) ); // Net Order Imbalance Indicator
+            case 'N': 
+                return onRpii( itch::rpii(record) ); // Retail Price Improvement Indicator
             case 'O':
                 return onDirectListingWithCapitalRaisePriceDiscovery( 
                         itch::direct_listing_with_capital_raise_price_discovery(record) );
